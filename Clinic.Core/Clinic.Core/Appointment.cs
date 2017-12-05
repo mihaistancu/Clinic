2c1d15144cef233e5a697cd034dacbebe40bd0c7
mihai <stancu_meehigh@yahoo.com>
2017-12-05 19:25:13 +0100

    Decided to make the appointment for a specific doctor

--- Clinic.Core/Clinic.Core/Appointment.cs
+++ Clinic.Core/Clinic.Core/Appointment.cs
@@ -4,6 +4,7 @@ namespace Clinic.Core
 {
     public class Appointment
     {
+        public Doctor Doctor { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
