2662ed3bb1b489e388ec3517d3f00107e01c9973
mihai <stancu_meehigh@yahoo.com>
2017-12-05 20:01:19 +0100

    Sample scheduling

---
+++ Clinic.Core/Clinic.Tests/SchedulingTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Clinic.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Clinic.Tests
+{
+    [TestClass]
+    public class SchedulingTests
+    {
+        [TestMethod]
+        public void Foo()
+        {
+            var schedule = new Schedule
+            {
+                Doctor = new Doctor
+                {
+                    Name = "Doctor",
+                    Practices = new List<Practice>
+                    {
+                        new Practice {Title = "Practice1"},
+                        new Practice {Title = "Practice2"}
+                    }
+                },
+                Office = new Office { Location = "Location" },
+                OfficeHours = new List<Availability>
+                {
+                    new Availability
+                    {
+                        DayOfWeek = DayOfWeek.Monday,
+                        StartTime = TimeSpan.Parse("08:00"),
+                        EndTime = TimeSpan.Parse("18:00")
+                    },
+                    new Availability
+                    {
+                        DayOfWeek = DayOfWeek.Tuesday,
+                        StartTime = TimeSpan.Parse("09:00"),
+                        EndTime = TimeSpan.Parse("19:00")
+                    }
+                }
+            };
+        }
+    }
+}
