20113c6e7ed2d53685c709c080fb8f2068629442
mihai <stancu_meehigh@yahoo.com>
2017-12-06 19:20:00 +0100

    Added medical records book

---
+++ Clinic.Core/Clinic.Core/MedicalRecordsBook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Clinic.Core
+{
+    public class MedicalRecordsBook
+    {
+        private readonly List<ClinicalVisit> visits;
+
+        public MedicalRecordsBook()
+        {
+            visits = new List<ClinicalVisit>();
+        }
+
+        public void Add(ClinicalVisit visit)
+        {
+            visits.Add(visit);    
+        }
+
+        public List<ClinicalVisit> SearchBy(Office office)
+        {
+            return visits.Where(v => v.Office == office).ToList();
+        }
+    }
+}
