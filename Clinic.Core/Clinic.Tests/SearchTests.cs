dfa5ca1f59e0e573d578c71b8c70d2d71dd8afce
mihai <stancu_meehigh@yahoo.com>
2017-12-06 19:45:30 +0100

    Refactoring

--- Clinic.Core/Clinic.Tests/SearchTests.cs
+++ Clinic.Core/Clinic.Tests/SearchTests.cs
@@ -11,27 +11,10 @@ namespace Clinic.Tests
         public void SearchByTimeInterval()
         {
             var office = new Office {Location = "Office1"};
-            var visit1 = new ClinicalVisit
-            {
-                Patient = new Patient {Name = "Patient1"},
-                Office = office,
-                StartDateTime = new DateTime(2010, 1, 1, 12, 0, 0),
-                EndDateTime = new DateTime(2010, 1, 1, 13, 0, 0)
-            };
-            var visit2 = new ClinicalVisit
-            {
-                Patient = new Patient { Name = "Patient2" },
-                Office = office,
-                StartDateTime = new DateTime(2010, 1, 1, 13, 0, 0),
-                EndDateTime = new DateTime(2010, 1, 1, 14, 0, 0)
-            };
-            var visit3 = new ClinicalVisit
-            {
-                Patient = new Patient {Name = "Patient3"},
-                Office = office,
-                StartDateTime = new DateTime(2010, 1, 1, 17, 0, 0),
-                EndDateTime = new DateTime(2010, 1, 1, 18, 0, 0)
-            };
+            var visit1 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 12, 0, 0), new DateTime(2010, 1, 1, 13, 0, 0));
+            var visit2 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 13, 0, 0), new DateTime(2010, 1, 1, 14, 0, 0));
+            var visit3 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 17, 0, 0), new DateTime(2010, 1, 1, 18, 0, 0));
+
             var medicalRecordsBoook = new MedicalRecordsBook();
             medicalRecordsBoook.Add(visit1);
             medicalRecordsBoook.Add(visit2);
@@ -42,6 +25,17 @@ namespace Clinic.Tests
                 new DateTime(2010, 1, 1, 15, 0, 0));
 
             CollectionAssert.AreEquivalent(new [] {visit1, visit2}, visits);
+        }
+        
+        private ClinicalVisit CreateClinicalVisit(Office office, DateTime startDateTime, DateTime endDateTime)
+        {
+            return new ClinicalVisit
+            {
+                Patient = new Patient { Name = "Patient" },
+                Office = office,
+                StartDateTime = startDateTime,
+                EndDateTime = endDateTime
+            };
         }
     }
 }
