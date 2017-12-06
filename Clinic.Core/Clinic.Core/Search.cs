c752fff8e0508052eb24625ad8fe19f0c237300a
mihai <stancu_meehigh@yahoo.com>
2017-12-06 19:10:25 +0100

    Adding search

---
+++ Clinic.Core/Clinic.Core/Search.cs
@@ -0,0 +1,7 @@
+﻿namespace Clinic.Core
+{
+    public class Search
+    {
+
+    }
+}
---
+++ Clinic.Core/Clinic.Tests/SearchTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Clinic.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Clinic.Tests
+{
+    [TestClass]
+    public class SearchTests
+    {
+        [TestMethod]
+        public void Foo()
+        {
+            var search = new Search();
+        }
+    }
+}
