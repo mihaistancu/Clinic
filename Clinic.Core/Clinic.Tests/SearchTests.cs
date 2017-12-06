7ffb7c2272141ef4505c75122f97b7598da8ac20
mihai <stancu_meehigh@yahoo.com>
2017-12-06 19:37:54 +0100

    Added test for searching by time interval

--- Clinic.Core/Clinic.Tests/SearchTests.cs
+++ Clinic.Core/Clinic.Tests/SearchTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Expressions;
+﻿using System;
 using Clinic.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,9 +8,40 @@ namespace Clinic.Tests
     public class SearchTests
     {
         [TestMethod]
-        public void Foo()
+        public void SearchByTimeInterval()
         {
-            var search = new Search();
+            var office = new Office {Location = "Office1"};
+            var visit1 = new ClinicalVisit
+            {
+                Patient = new Patient {Name = "Patient1"},
+                Office = office,
+                StartDateTime = new DateTime(2010, 1, 1, 12, 0, 0),
+                EndDateTime = new DateTime(2010, 1, 1, 13, 0, 0)
+            };
+            var visit2 = new ClinicalVisit
+            {
+                Patient = new Patient { Name = "Patient2" },
+                Office = office,
+                StartDateTime = new DateTime(2010, 1, 1, 13, 0, 0),
+                EndDateTime = new DateTime(2010, 1, 1, 14, 0, 0)
+            };
+            var visit3 = new ClinicalVisit
+            {
+                Patient = new Patient {Name = "Patient3"},
+                Office = office,
+                StartDateTime = new DateTime(2010, 1, 1, 17, 0, 0),
+                EndDateTime = new DateTime(2010, 1, 1, 18, 0, 0)
+            };
+            var medicalRecordsBoook = new MedicalRecordsBook();
+            medicalRecordsBoook.Add(visit1);
+            medicalRecordsBoook.Add(visit2);
+            medicalRecordsBoook.Add(visit3);
+
+            var visits = medicalRecordsBoook.SearchBy(office, 
+                new DateTime(2010, 1, 1, 10, 0, 0),
+                new DateTime(2010, 1, 1, 15, 0, 0));
+
+            CollectionAssert.AreEquivalent(new [] {visit1, visit2}, visits);
         }
     }
 }
