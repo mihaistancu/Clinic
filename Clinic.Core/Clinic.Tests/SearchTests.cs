ee35811c7f340bb5acbda6cd31dc40baa9340110
mihai <stancu_meehigh@yahoo.com>
2017-12-06 19:49:30 +0100

    Extended the test for searching by time interval

--- Clinic.Core/Clinic.Tests/SearchTests.cs
+++ Clinic.Core/Clinic.Tests/SearchTests.cs
@@ -11,20 +11,22 @@ namespace Clinic.Tests
         public void SearchByTimeInterval()
         {
             var office = new Office {Location = "Office1"};
-            var visit1 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 12, 0, 0), new DateTime(2010, 1, 1, 13, 0, 0));
-            var visit2 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 13, 0, 0), new DateTime(2010, 1, 1, 14, 0, 0));
-            var visit3 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 17, 0, 0), new DateTime(2010, 1, 1, 18, 0, 0));
+            var visit1 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 9, 0, 0), new DateTime(2010, 1, 1, 10, 0, 0));
+            var visit2 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 12, 0, 0), new DateTime(2010, 1, 1, 13, 0, 0));
+            var visit3 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 13, 0, 0), new DateTime(2010, 1, 1, 14, 0, 0));
+            var visit4 = CreateClinicalVisit(office, new DateTime(2010, 1, 1, 17, 0, 0), new DateTime(2010, 1, 1, 18, 0, 0));
 
             var medicalRecordsBoook = new MedicalRecordsBook();
             medicalRecordsBoook.Add(visit1);
             medicalRecordsBoook.Add(visit2);
             medicalRecordsBoook.Add(visit3);
+            medicalRecordsBoook.Add(visit4);
 
             var visits = medicalRecordsBoook.SearchBy(office, 
-                new DateTime(2010, 1, 1, 10, 0, 0),
+                new DateTime(2010, 1, 1, 11, 0, 0),
                 new DateTime(2010, 1, 1, 15, 0, 0));
 
-            CollectionAssert.AreEquivalent(new [] {visit1, visit2}, visits);
+            CollectionAssert.AreEquivalent(new [] {visit2, visit3}, visits);
         }
         
         private ClinicalVisit CreateClinicalVisit(Office office, DateTime startDateTime, DateTime endDateTime)
