25ad9c3f23487749ad92ebaae6092a53326ecefc
mihai <stancu_meehigh@yahoo.com>
2017-12-07 21:36:06 +0100

    Added report for clinic activity

---
+++ Clinic.Core/Clinic.Core/TotalActivityReport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Clinic.Core
+{
+    public class TotalActivityReport
+    {
+        public DateTime StartDateTime { get; private set; }
+        public DateTime EndDateTime { get; private set; }
+        public TimeSpan TotalVisitsDuration { get; private set; }
+        public double TotalAmountReceived { get; private set; }
+
+        private TotalActivityReport() { }
+
+        public static TotalActivityReport For(MedicalRecordsBook medicalRecordsBook, DateTime startDateTime, DateTime endDateTime)
+        {
+            var visits = medicalRecordsBook.Search(Filter.ByTimeframe(startDateTime, endDateTime));
+
+            return new TotalActivityReport
+            {
+                StartDateTime = startDateTime,
+                EndDateTime = endDateTime,
+                TotalVisitsDuration = new TimeSpan(visits.Sum(v => v.Duration.Ticks)),
+                TotalAmountReceived = visits.Sum(v => v.AmountReceived)
+            };
+        }
+    }
+}
