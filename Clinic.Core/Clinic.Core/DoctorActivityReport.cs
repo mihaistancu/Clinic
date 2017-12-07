e27417ee2dec19aba6899fb4c5a9c2b5734a175d
mihai <stancu_meehigh@yahoo.com>
2017-12-07 20:53:59 +0100

    Added doctor activity report

---
+++ Clinic.Core/Clinic.Core/DoctorActivityReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Clinic.Core
+{
+    public class DoctorActivityReport
+    {
+        public int ClinicalVisitsCount { get; private set; }
+
+        public double TotalAmountReceived { get; private set; }
+        
+        private DoctorActivityReport() { }
+
+        public static DoctorActivityReport For(MedicalRecordsBook medicalRecordsBook, Doctor doctor, DateTime startDateTime, DateTime endDateTime)
+        {
+            var visits = medicalRecordsBook.Search(Filter.ByDoctor(doctor), Filter.ByTimeframe(startDateTime, endDateTime));
+
+            return new DoctorActivityReport
+            {
+                ClinicalVisitsCount = visits.Count,
+                TotalAmountReceived = visits.Sum(v => v.AmountReceived)
+            };
+        }
+    }
+}
