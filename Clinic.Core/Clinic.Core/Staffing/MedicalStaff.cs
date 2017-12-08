f7fce99e1a3e7529d4b9d1d77fff5902acc8deff
mihaistancu <stancu_meehigh@yahoo.com>
2017-12-09 00:14:54 +0100

    Added class for medical staff

---
+++ Clinic.Core/Clinic.Core/Staffing/MedicalStaff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Clinic.Core.Staffing
+{
+    public class MedicalStaff
+    {
+        private List<Doctor> doctors;
+
+        public void Add(Doctor doctor)
+        {
+            doctors.Add(doctor);
+        }
+    }
+}
