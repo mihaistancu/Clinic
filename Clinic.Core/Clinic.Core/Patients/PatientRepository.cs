32730442ff533386764fc747b384016e8953a766
mihaistancu <stancu_meehigh@yahoo.com>
2017-12-09 00:25:12 +0100

    Added patient repository

---
+++ Clinic.Core/Clinic.Core/Patients/PatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Clinic.Core.Patients
+{
+    public class PatientRepository
+    {
+        private List<Patient> patients;
+
+        public void Add(Patient patient)
+        {
+            patients.Add(patient);
+        }
+    }
+}
