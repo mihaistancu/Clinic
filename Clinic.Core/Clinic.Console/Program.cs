5413e64551bbf6f3e1e11312359c120642d0bc15
mihaistancu <stancu_meehigh@yahoo.com>
2017-12-09 00:36:04 +0100

    Add a new doctor to the in-memory list of doctors in the console application

--- Clinic.Core/Clinic.Console/Program.cs
+++ Clinic.Core/Clinic.Console/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Clinic.Console
+﻿using Clinic.Core.Staffing;
+
+namespace Clinic.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var medicalStaff = new MedicalStaff();
+
+            string doctorName = args[0];
+
+            medicalStaff.Add(new Doctor { Name = doctorName });
         }
     }
 }
