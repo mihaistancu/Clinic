94fb151ca45c04267370f75544cb77d538558ff2
mihai <stancu_meehigh@yahoo.com>
2017-12-11 21:36:20 +0100

    Added DbContext and an ID for doctors

---
+++ Clinic.Core/Clinic.DataAccess/ClinicDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Clinic.Core.Staffing;
+
+namespace Clinic.DataAccess
+{
+    public class ClinicDbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; } 
+    }
+}
