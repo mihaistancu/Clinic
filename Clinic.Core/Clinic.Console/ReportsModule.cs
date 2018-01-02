80240977508ae42e9ad6f231f5797036e57d51fc
mihaistancu <stancu_meehigh@yahoo.com>
2018-01-02 18:10:08 +0100

    Allow viewing clinic activity reports

--- Clinic.Core/Clinic.Console/ReportsModule.cs
+++ Clinic.Core/Clinic.Console/ReportsModule.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Clinic.Core.Reports;
+using Clinic.DataAccess;
+using System;
 
 namespace Clinic.Console
 {
     public class ReportsModule : IModule
     {
+        ConsultationsRepository repository;
+
+        public ReportsModule()
+        {
+            repository = new ConsultationsRepository();
+        }
+
         public void Execute(string[] args)
         {
-            throw new NotImplementedException();
+            switch (args[1])
+            {
+                case "clinic": ViewClinicActivity(args); break;
+            }
+        }
+
+        private void ViewClinicActivity(string[] args)
+        {
+            var startDateTime = DateTime.Parse(args[2]);
+            var endDateTime = DateTime.Parse(args[3]);
+
+            var report = ClinicActivityReport.For(repository, startDateTime, endDateTime);
+
+            System.Console.WriteLine("Total visits duration: {0}", report.TotalVisitsDuration);
+            System.Console.WriteLine("Total amount received: {0}", report.TotalAmountReceived);
         }
     }
 }
