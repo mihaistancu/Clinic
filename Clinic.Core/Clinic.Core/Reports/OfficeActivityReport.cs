66f99d9a11e36e5a85ee8095ec627238790d4435
mihaistancu <stancu_meehigh@yahoo.com>
2018-01-02 19:03:07 +0100

    Use a date range for office activity reports as well since it is more general

--- Clinic.Core/Clinic.Core/Reports/OfficeActivityReport.cs
+++ Clinic.Core/Clinic.Core/Reports/OfficeActivityReport.cs
@@ -8,19 +8,21 @@ namespace Clinic.Core.Reports
     public class OfficeActivityReport
     {
         public Office Office { get; private set; }
-        public DateTime Date { get; private set; }
+        public DateTime StartDateTime { get; private set; }
+        public DateTime EndDateTime { get; private set; }
         public TimeSpan TotalVisitDuration { get; private set; }
         
         private OfficeActivityReport() { }
 
-        public static OfficeActivityReport For(IRepository<ClinicalVisit> medicalRecordsBook, Office office, DateTime date)
+        public static OfficeActivityReport For(IRepository<ClinicalVisit> medicalRecordsBook, Office office, DateTime startDateTime, DateTime endDateTime)
         {
-            var visits = medicalRecordsBook.Search(Filter.ByOffice(office), Filter.ByDate(date));
+            var visits = medicalRecordsBook.Search(Filter.ByOffice(office), Filter.ByTimeframe(startDateTime, endDateTime));
 
             return new OfficeActivityReport
             {
                 Office = office,
-                Date = date,
+                StartDateTime = startDateTime,
+                EndDateTime = endDateTime,
                 TotalVisitDuration = new TimeSpan(visits.Sum(v => v.Duration.Ticks))
             };
         }
