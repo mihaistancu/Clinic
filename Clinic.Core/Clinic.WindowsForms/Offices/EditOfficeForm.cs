ed456d708551da7c95f508141378b43daaa89ca2
mihaistancu <stancu_meehigh@yahoo.com>
2018-01-02 22:51:35 +0100

    Remove unused using statements

--- Clinic.Core/Clinic.WindowsForms/Offices/EditOfficeForm.cs
+++ Clinic.Core/Clinic.WindowsForms/Offices/EditOfficeForm.cs
@@ -1,13 +1,6 @@
 ﻿using Clinic.Core.Infrastructure;
 using Clinic.DataAccess;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Clinic.WindowsForms.Offices
