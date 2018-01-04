2822cb9759f48e58b837e34c550a9f705a795018
mihai <stancu_meehigh@yahoo.com>
2018-01-04 21:57:52 +0100

    Refactoring

--- Clinic.Core/Clinic.WindowsForms/OfficeHours/OfficeHoursForm.cs
+++ Clinic.Core/Clinic.WindowsForms/OfficeHours/OfficeHoursForm.cs
@@ -42,9 +42,10 @@ namespace Clinic.WindowsForms.OfficeHours
         private void LoadOfficesHours()
         {
             var officeHoursRepository = new OfficeHoursRepository();
-            var results = from x in officeHoursRepository.Search(GetSearchCriteria())
-                from h in x.OfficeHours
-                select new
+            var searchPredicates = GetSearchPredicates().ToArray();
+
+            var results = from x in officeHoursRepository.Search(searchPredicates)
+                from h in x.OfficeHours select new
                 {
                     Doctor = x.Doctor.Name,
                     Office = x.Office.Location,
@@ -56,21 +57,17 @@ namespace Clinic.WindowsForms.OfficeHours
             officeHoursDataGridView.DataSource = results.ToList();
         }
 
-        private Expression<Func<WeeklyOfficeHours, bool>>[] GetSearchCriteria()
+        private IEnumerable<Expression<Func<WeeklyOfficeHours, bool>>> GetSearchPredicates()
         {
-            var predicates = new List<Expression<Func<WeeklyOfficeHours, bool>>>();
-
             if (!string.IsNullOrEmpty(doctorComboBox.Text))
             {
-                predicates.Add(h => h.Doctor.Name == doctorComboBox.Text);
+                yield return h => h.Doctor.Name == doctorComboBox.Text;
             }
 
             if (!string.IsNullOrEmpty(officeComboBox.Text))
             {
-                predicates.Add(h => h.Office.Location == officeComboBox.Text);
+                yield return h => h.Office.Location == officeComboBox.Text;
             }
-
-            return predicates.ToArray();
         }
 
         private void OnAddButtonClicked(object sender, EventArgs e)
