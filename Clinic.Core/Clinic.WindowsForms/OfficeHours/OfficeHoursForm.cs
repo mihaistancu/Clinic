44950bc876dda2e1b62c2f79609b087ec8a228e8
mihai <stancu_meehigh@yahoo.com>
2018-01-04 22:01:14 +0100

    Remove unused using statements

--- Clinic.Core/Clinic.WindowsForms/OfficeHours/OfficeHoursForm.cs
+++ Clinic.Core/Clinic.WindowsForms/OfficeHours/OfficeHoursForm.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Clinic.Core.Infrastructure;
 using Clinic.Core.OfficeHours;
