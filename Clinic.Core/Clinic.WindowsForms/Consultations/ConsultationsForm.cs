afe5bc6bf523a64468b594464d18181393d8570b
mihaistancu <stancu_meehigh@yahoo.com>
2018-01-06 17:25:45 +0100

    Load combos on consultations form

--- Clinic.Core/Clinic.WindowsForms/Consultations/ConsultationsForm.cs
+++ Clinic.Core/Clinic.WindowsForms/Consultations/ConsultationsForm.cs
@@ -1,4 +1,7 @@
-﻿using Clinic.DataAccess;
+﻿using Clinic.Core.Infrastructure;
+using Clinic.Core.Patients;
+using Clinic.Core.Staffing;
+using Clinic.DataAccess;
 using System;
 using System.Windows.Forms;
 
@@ -9,12 +12,36 @@ namespace Clinic.WindowsForms.Consultations
         public ConsultationsForm()
         {
             InitializeComponent();
+            LoadDoctors();
+            LoadOffices();
+            LoadPatients();
             ReloadData();
         }
 
         private void OnAddButtonClicked(object sender, EventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        private void LoadDoctors()
+        {
+            var doctorRepository = new Repository<Doctor>();
+            var doctors = doctorRepository.GetAll();
+            doctorComboBox.DataSource = doctors;
+        }
+
+        private void LoadOffices()
+        {
+            var officeRepository = new Repository<Office>();
+            var offices = officeRepository.GetAll();
+            officeComboBox.DataSource = offices;
+        }
+
+        private void LoadPatients()
+        {
+            var patientsRepository = new Repository<Patient>();
+            var patients = patientsRepository.GetAll();
+            patientComboBox.DataSource = patients;
         }
 
         private void ReloadData()
