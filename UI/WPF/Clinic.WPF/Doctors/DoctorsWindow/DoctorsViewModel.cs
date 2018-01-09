e670e48d1ea66273b1af40549ffc9df64f5dce59
mihaistancu <stancu_meehigh@yahoo.com>
2018-01-09 03:35:15 +0100

    View doctors

--- UI/WPF/Clinic.WPF/Doctors/DoctorsWindow/DoctorsViewModel.cs
+++ UI/WPF/Clinic.WPF/Doctors/DoctorsWindow/DoctorsViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Clinic.WPF.Doctors.EditDoctorWindow;
+﻿using System.Collections.Generic;
+using Clinic.Data;
+using Clinic.Data.Persistence.EF;
+using Clinic.WPF.Doctors.EditDoctorWindow;
 using Clinic.WPF.Mvvm;
 
 namespace Clinic.WPF.Doctors.DoctorsWindow
@@ -7,9 +10,14 @@ namespace Clinic.WPF.Doctors.DoctorsWindow
     {
         public ShowCommand<EditDoctorViewModel> ShowEditDoctorViewCommand { get; set; }
 
+        public List<Doctor> Doctors { get; private set; }
+
         public DoctorsViewModel()
         {
             ShowEditDoctorViewCommand = new ShowCommand<EditDoctorViewModel>();
+
+            var repository = new Repository<Doctor>();
+            Doctors = repository.GetAll();
         }
     }
 }
