0524d2f9e1dd9fdac9aa3e1b5583e943d856ead3
mihaistancu <stancu_meehigh@yahoo.com>
2018-01-11 00:51:17 +0100

    Load doctors only when the window is loaded, to prevent issues with the Visual Studio designer (the viewmodel constructor is called)

---
+++ UI/WPF/Clinic.WPF.Mvvm/ExecuteCommandOnLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Clinic.WPF.Mvvm
+{
+    public class ExecuteCommandOnLoad: Behavior<Window>
+    {
+        public static DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command", typeof(Command), typeof(ExecuteCommandOnLoad));
+
+        public Command Command
+        {
+            get { return (Command)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (Command.CanExecute(e))
+            {
+                Command.Execute(e);
+            }
+        }
+    }
+}
--- UI/WPF/Clinic.WPF/Doctors/DoctorsWindow/DoctorsViewModel.cs
+++ UI/WPF/Clinic.WPF/Doctors/DoctorsWindow/DoctorsViewModel.cs
@@ -9,13 +9,18 @@ namespace Clinic.WPF.Doctors.DoctorsWindow
     public class DoctorsViewModel
     {
         public ShowCommand<EditDoctorViewModel> ShowEditDoctorViewCommand { get; set; }
+        public Command LoadDoctorsCommand { get; set; }
 
         public List<Doctor> Doctors { get; private set; }
 
         public DoctorsViewModel()
         {
             ShowEditDoctorViewCommand = new ShowCommand<EditDoctorViewModel>();
+            LoadDoctorsCommand = new Command(LoadDoctors);
+        }
 
+        private void LoadDoctors()
+        {
             var repository = new Repository<Doctor>();
             Doctors = repository.GetAll();
         }
