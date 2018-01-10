7f384c3ec4328c142d1941406e7f9824b6cd4df2
mihaistancu <stancu_meehigh@yahoo.com>
2018-01-10 23:48:31 +0100

    Populate view and viewmodel for adding a doctor

--- UI/WPF/Clinic.WPF/Doctors/EditDoctorWindow/EditDoctorViewModel.cs
+++ UI/WPF/Clinic.WPF/Doctors/EditDoctorWindow/EditDoctorViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace Clinic.WPF.Doctors.EditDoctorWindow
+﻿using Clinic.WPF.Mvvm;
+
+namespace Clinic.WPF.Doctors.EditDoctorWindow
 {
     public class EditDoctorViewModel
     {
+        public string DoctorName { get; set; }
+
+        public Command SaveDoctorCommand { get; set; }
+
+        public EditDoctorViewModel()
+        {
+            SaveDoctorCommand = new Command(SaveDoctor);
+        }
+
+        private void SaveDoctor()
+        {
+        }
     }
 }
